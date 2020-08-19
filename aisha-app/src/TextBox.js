173d2b9d17012cb7337e03d9686fd5b93cf540dd
Arav <arav.patel@gmail.com>
2020-08-19 16:27:49 +0100

    Added response function that is called when the form is submitted

--- aisha-app/src/TextBox.js
+++ aisha-app/src/TextBox.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 
 class TextBox extends React.Component{
   constructor(props){
     super(props)
+
+    this.state = ({
+      formSubmit: false
+    })
+
+    this.onSubmit=this.onSubmit.bind(this);
   }
 
+  onSubmit(event){
+    event.preventDefault();
+    this.setState({
+      formSubmit: true
+    })
+    this.props.submit();
+  }
+
+  /* create a function onSubmit
+  - onSubmit triggers a state change
+  - state change triggers a re-render to include response testid
+  */
+
   render() {
+    let response = null;
+
+    if(this.state.formSubmit === true){
+      response = (
+        <div>
+          <p data-testid="response">
+            Thanks for letting me know
+          </p>
+        </div>
+      )
+    }
     return(
-      <form onSubmit={this.props.submit}>
-        <label>Input
-          <input type="text"></input>
-        </label>
-        <input type="submit" value="Submit"></input>
-      </form>
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Input
+            <input type="text"></input>
+          </label>
+          <input type="submit" value="Submit"></input>
+        </form>
+        {response}
+      </div>
      );
   }
 }
--- aisha-app/src/__tests__/TextBox.js
+++ aisha-app/src/__tests__/TextBox.js
@@ -4,12 +4,25 @@ import {render, fireEvent, screen, getByTestId} from '@testing-library/react'
 import TextBox from '../TextBox'
 
 test('onSubmit is called when the form submits', () => {
+  const originalError = console.error;
+  console.error = jest.fn();
+
   const onSubmit = jest.fn() /* is a mock function */
   const {getByLabelText } = render(<TextBox submit={onSubmit}/>);
   
+  fireEvent.click(screen.getByText('Submit'))
+  expect(onSubmit).toHaveBeenCalled();
+  
+  console.error = originalError;
+})
+
+test('onSubmit changes the screen when form is submitted', () => {
+  const onSubmit = jest.fn() /* is a mock function */
+  const { getByLabelText, getByTestId } = render(<TextBox submit={onSubmit}/>);
 
   fireEvent.change(getByLabelText("Input"), {target: {value: "I am having a fantastic day, thank you for asking"}})
-  fireEvent.click(screen.getByText('Submit'))
-  console.log(screen)
-  expect(onSubmit).toHaveBeenCalled();
+
+  fireEvent.click(screen.getByText("Submit"))
+
+  expect(getByTestId("response")).toHaveTextContent("Thanks for letting me know")
 })
