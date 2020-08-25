14e37dc6ea4f778dc2a5dc38d0e7dbaf3166e56b
Arav <arav.patel@gmail.com>
2020-08-25 11:48:49 +0100

    Integrated emotion analysis tool into the main model

--- flask-server/bot-tests.py
+++ flask-server/bot-tests.py
@@ -11,10 +11,10 @@ class BotTest(unittest.TestCase):
         self.assertEqual(type(self.testBot), bot.Bot) 
 
     def test_02_test_positive_response(self):
-        self.assertIn(self.testBot.analyse("Happy")['bot_response'], self.testBot.happy_response)
+        self.assertIn(self.testBot.analyse("Happy")['bot_response'], self.testBot.Joy)
 
     def test_03_test_negative_response(self):
-        self.assertIn(self.testBot.analyse("Sad")['bot_response'], self.testBot.sad_response)  
+        self.assertIn(self.testBot.analyse("Sad")['bot_response'], self.testBot.Sad)  
 
 if __name__ == "__main__":
     unittest.main()
